26f21aeb1a246222fca52019f8297610856f3116
Snow4k <devinb4k@gmail.com>
2018-12-08 23:53:45 -0500

    Added Routing and contact page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home/home.component'
+import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
   {path:"home", component: HomeComponent },
-  {path:"", redirectTo:'./home', pathMatch:'full'}
+  {path:"contact", component: ContactComponent,},
+  {path:"", redirectTo:'/home', pathMatch:'full'}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {ScrollToModule} from '@nicky-lenaers/ngx-scroll-to';
 import {NgsRevealModule} from 'ngx-scrollreveal';
+import { FormsModule }   from '@angular/forms';
 
 
 import { AppRoutingModule } from './app-routing.module';
@@ -9,6 +10,7 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
 
 
 @NgModule({
@@ -17,6 +19,8 @@ import { HomeComponent } from './home/home.component';
     NavbarComponent,
     FooterComponent,
     HomeComponent,
+    ContactComponent,
+    
     
   ],
   imports: [
@@ -24,6 +28,7 @@ import { HomeComponent } from './home/home.component';
     BrowserModule,
     AppRoutingModule,
     NgsRevealModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
