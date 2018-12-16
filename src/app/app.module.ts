70398366f9b4763860c83b77c21f473576a18d24
Snow4k <devinb4k@gmail.com>
2018-12-16 14:37:23 -0500

    Build V3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import {ScrollToModule} from '@nicky-lenaers/ngx-scroll-to';
 import {NgsRevealModule} from 'ngx-scrollreveal';
 import { FormsModule }   from '@angular/forms';
+import { NgCircleProgressModule } from 'ng-circle-progress';
 
 
 import { AppRoutingModule } from './app-routing.module';
@@ -29,6 +30,26 @@ import { ContactComponent } from './contact/contact.component';
     AppRoutingModule,
     NgsRevealModule,
     FormsModule,
+    NgCircleProgressModule.forRoot({
+      "backgroundColor": "#f3fbff",
+      "backgroundStrokeWidth": 50,
+      "radius": 45,
+      "space": -6,
+      "toFixed": 0,
+      "maxPercent": 100,
+      "outerStrokeWidth": 10,
+      "outerStrokeColor": "#00e4ff",
+      "innerStrokeColor": "#c4f8ff",
+      "innerStrokeWidth": 8,
+      "titleColor": "#5e5e5e",
+      "titleFontSize": "14",
+      "animateTitle": true,
+      "animationDuration": 1000,
+      "showUnits": false,
+      "showBackground": false,
+      "clockwise": false,
+      "responsive": true
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
